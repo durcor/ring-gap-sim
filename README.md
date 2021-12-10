bef0737435aac556746f7557f1d5457024e64627
Tyler Kaminski <durcor@disroot.org>
2021-12-10 14:46:03 -0500

    Add README and work on generalizing for particles

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simulating the Ring Gaps of Saturn
+
+# Pseudo-code
+1. Store the x-y coordinates and masses of Saturn's moons
+2. Store x-y coordinates of particles in Saturn's rings
+3. Use Euler's method/RK4 to simulate the motion of each particle around Saturn in the presence of Saturn's moons
--- sim_ring_gaps.py
+++ sim_ring_gaps.py
@@ -22,7 +22,9 @@ m = 3.7493e19
 # total time of simulation (in seconds)
 TIME = 100000000000000
 
-particles = [[np.random.randint(-100, 100), np.random.randint(-100, 100)]
+lim = 13000000000
+
+particles = [[np.random.randint(-lim, lim), np.random.randint(-lim, lim)]
              for _ in range(1000)]
 
 
@@ -39,61 +41,57 @@ def get_ks(vx, vy, x, y, vxo, vyo, xo, yo, t):
 
 # Simulate an orbit using a certain orbital approximation method.
 def orbit(method, t, x, y, vx, vy):
+    # Simulate the euler, rk2, and rk4 methods based on input.
+    if method == 'euler':
+        r = np.sqrt(float(x)**2 + float(y)**2)
+
+        ax = -G * M * (x/r**3)
+        ay = -G * M * (y/r**3)
+
+        x += vx * t + ax/2 * t**2
+        y += vy * t + ay/2 * t**2
+
+        vx += ax * t
+        vy += ay * t
+    elif method == 'rk2':
+        k1x, k1y, k1vx, k1vy = get_ks(vx, vy, x, y,
+                                      0, 0, 0, 0, t)
+        k2x, k2y, k2vx, k2vy = get_ks(vx, vy, x, y,
+                                      k1vx, k1vy, k1x, k1y, t)
+
+        x += (k1x + k2x)/2
+        y += (k1y + k2y)/2
+
+        vx += (k1vx + k2vx)/2
+        vy += (k1vy + k2vy)/2
+    elif method == 'rk4':
+        k1x, k1y, k1vx, k1vy = get_ks(vx, vy, x, y,
+                                      0, 0, 0, 0, t)
+        k2x, k2y, k2vx, k2vy = get_ks(vx, vy, x, y,
+                                      k1vx/2, k1vy/2, k1x/2, k1y/2, t)
+        k3x, k3y, k3vx, k3vy = get_ks(vx, vy, x, y,
+                                      k2vx/2, k2vy/2, k2x/2, k2y/2, t)
+        k4x, k4y, k4vx, k4vy = get_ks(vx, vy, x, y,
+                                      k3vx, k3vy, k3x, k3y, t)
+
+        x += (k1x + 2 * k2x + 2 * k3x + k4x)/6
+        y += (k1y + 2 * k2y + 2 * k3y + k4y)/6
+
+        vx += (k1vx + 2 * k2vx + 2 * k3vx + k4vx)/6
+        vy += (k1vy + 2 * k2vy + 2 * k3vy + k4vy)/6
     plt.plot(x, y, 'ro')
-    for _ in range(TIME):
-        # Simulate the euler, rk2, and rk4 methods based on input.
-        if method == 'euler':
-            r = np.sqrt(x**2 + y**2)
-
-            ax = -G * M * (x/r**3)
-            ay = -G * M * (y/r**3)
-
-            x += vx * t + ax/2 * t**2
-            y += vy * t + ay/2 * t**2
-
-            vx += ax * t
-            vy += ay * t
-        elif method == 'rk2':
-            k1x, k1y, k1vx, k1vy = get_ks(vx, vy, x, y,
-                                          0, 0, 0, 0, t)
-            k2x, k2y, k2vx, k2vy = get_ks(vx, vy, x, y,
-                                          k1vx, k1vy, k1x, k1y, t)
-
-            x += (k1x + k2x)/2
-            y += (k1y + k2y)/2
-
-            vx += (k1vx + k2vx)/2
-            vy += (k1vy + k2vy)/2
-        elif method == 'rk4':
-            k1x, k1y, k1vx, k1vy = get_ks(vx, vy, x, y,
-                                          0, 0, 0, 0, t)
-            k2x, k2y, k2vx, k2vy = get_ks(vx, vy, x, y,
-                                          k1vx/2, k1vy/2, k1x/2, k1y/2, t)
-            k3x, k3y, k3vx, k3vy = get_ks(vx, vy, x, y,
-                                          k2vx/2, k2vy/2, k2x/2, k2y/2, t)
-            k4x, k4y, k4vx, k4vy = get_ks(vx, vy, x, y,
-                                          k3vx, k3vy, k3x, k3y, t)
-
-            x += (k1x + 2 * k2x + 2 * k3x + k4x)/6
-            y += (k1y + 2 * k2y + 2 * k3y + k4y)/6
-
-            vx += (k1vx + 2 * k2vx + 2 * k3vx + k4vx)/6
-            vy += (k1vy + 2 * k2vy + 2 * k3vy + k4vy)/6
-        # Plot each point and add its distance's absolute values to their
-        # arrays so that eccentricity can be found.
-        plt.plot(x, y, 'ro')
+    plt.draw()
+    plt.pause(0.0000001)
 
 
 # Make the scales for the plots equal on both axes.
 plt.gca().set_aspect('equal')
 
-# Simulate each method using 29.8km/s, 42.1km/s, and 32.7km/s as inputs for
-# the initial vy.
 for method in ['euler', 'rk2', 'rk4']:
     # Start each orbit at Earth's location.
     t = TIME / 1e5
-    vx = 0
-    for p in particles:
-        orbit(method, t, p[0], p[1], vx, vy=29.8e3)
-    plt.savefig(method + ".png")
-    plt.cla()
+    for _ in range(TIME):
+        for x, y in particles:
+            plt.plot(x, y, 'ro')
+            orbit(method, t, x, y, 0, 0)
+        plt.cla()
