Work on fixing velocity calculation

sim_ring_gaps.py
@@ -21,16 +21,20 @@ M = 5.6834e26
 m = 3.7493e19 * 10
 # Distance from Saturn to Mimas
 d = 1.8552e8
-LIM = 13000000000
 method = 'euler'
 # time step (length of one orbit of Mimas in seconds)
-t = 60 * 60 * 22.6
-# total time of simulation
-TIME = t * 1000
+t = int(60 * 60 * 22.6)
+# total time of simulation (in mimas days)
+TIME = 2000 * t
 
+INNER_LIM = 11000000000
+OUTER_LIM = 13000000000
 NUM_PARTICLES = 1000
+
 # particles = np.random.randint(-LIM, LIM, (NUM_PARTICLES, 2))
-p = np.array([(float(i), 0.0) for i in range(110000000, 130000000, 5000000)])
+p = np.array([(float(i), 0.0) for i in range(INNER_LIM, OUTER_LIM,
+                                             (OUTER_LIM - INNER_LIM) // NUM_PARTICLES)])
+v = np.array([(0, np.sqrt(G * M / x)) for x, _ in p])
 
 
 # General case for a single step in the RK# methods.
@@ -88,33 +92,27 @@ def orbit(method, t, x, y, vx, vy):
     plot.scatter(x, y)
 
 
-# Make the scales for the plots equal on both axes.
-# TODO: Start Mimas at x=0
-# Start all particles at x=0
+plot.gca().set_aspect('equal')
+# array for initial velocity of every particle
 
 # Plot Saturn
 plot.plot(0, 0, 'yo')
-
-# Orbit simulation method to use. Avialable methods: 'euler', 'rk2', and 'rk4'
 w = np.sqrt(G * M / d**3)
 for n in range(0, TIME, t):
-    xm = d * np.cos(n * t)
-    ym = d * np.sin(n * t)
-    # plot.plot(xm, ym, 'go')
-    vo = np.sqrt(G * M / d)
-    vx = -vo * np.sin(n * t)
-    vy = vo * np.cos(n * t)
+    xm = d * np.cos(n * w)
+    ym = d * np.sin(n * w)
+    plot.plot(xm, ym, 'go')
     for i in range(len(p)):
         rs = np.sqrt(p[i][0]**2 + p[i][1]**2)
         rm = np.sqrt((p[i][0] - xm)**2 + (p[i][1] - ym)**2)
 
-        ax = -G * M * (p[i][0]/rs**3) - G * m * (p[i][0] - xm)/rm**3
-        ay = -G * M * (p[i][1]/rs**3) - G * m * (p[i][1] - ym)/rm**3
+        ax = (-G * M * p[i][0]/rs**3) - ((G * m * (p[i][0] - xm)/rm**3))
+        ay = (-G * M * p[i][1]/rs**3) - ((G * m * (p[i][1] - ym)/rm**3))
 
-        p[i][0] += vx * t + ax/2 * t**2
-        p[i][1] += vy * t + ay/2 * t**2
-        vx += ax * t
-        vy += ay * t
+        p[i][0] += v[i][0] * t + ax/2 * t**2
+        p[i][1] += v[i][1] * t + ay/2 * t**2
+        v[i][0] += ax * t
+        v[i][1] += ay * t
         plot.plot(p[i][0], p[i][1], 'ro')
-        plot.draw()
-        plot.pause(0.000000001)
+    plot.pause(0.00000001)
+# plot.savefig('ring_gap.png')