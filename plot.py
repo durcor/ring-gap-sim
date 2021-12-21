c76a7b894baa15413b245895362b8f00778e8e2d
Tyler Kaminski <durcor@disroot.org>
2021-12-21 16:25:08 -0500

    Add more plots and make them pretty

--- plot.py
+++ plot.py
@@ -14,9 +14,16 @@ subprocess.run("./sim_ring_gaps " + ' '.join(sys.argv[1:]) + " > data.csv",
 data = csv.reader(open("data.csv"))
 xm, ym = next(data)
 plot.gca().set_aspect('equal')
-plot.plot(float(xm), float(ym), "ro")
-plot.plot(0, 0, "yo")
+plot.title("p=" + sys.argv[1] + ", m=" + sys.argv[2] +
+           ", t=" + sys.argv[3])
+plot.xlabel("Distance from Saturn (m)")
+plot.ylabel("Distance from Saturn (m)")
+plot.plot(float(xm), float(ym), "ro", label="Mimas")
+plot.plot(0, 0, "yo", markersize=20, label="Saturn")
+x, y = next(data)
+plot.plot(float(x), float(y), "go", markersize=1, label="Ring Particle")
 for x, y in data:
     plot.plot(float(x), float(y), "go", markersize=1)
+plot.legend(loc='upper right')
 plot.savefig('img/mimas_mult=' + sys.argv[2] + '&p=' + sys.argv[1] + '&t=' +
              sys.argv[3] + '.png')
